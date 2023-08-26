3f68b66a36d103f3ea3aa99c677136eb2aaeb0b9
WhyK <windchimeyk@gmail.com>
2023-08-27 02:12:38 +0900

    test: add no OGP case

--- mod.test.ts
+++ mod.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.188.0/testing/asserts.ts";
 import { stub } from "https://deno.land/std@0.188.0/testing/mock.ts";
 import { type MetaData, parsedMeta } from "./mod.ts";
 
-const HTML_DATA = `
+const FULL_OGP_HTML_DATA = `
 <html>
   <head>
     <title>テスト</title>
@@ -23,40 +23,93 @@ const HTML_DATA = `
   <body>テスト</body>
 </html>
 `;
+const NO_OGP_HTML_DATA = `
+<html>
+  <head>
+    <title>テスト</title>
+    <meta name="description" content="テスト">
+    <link rel="shortcut icon" href="/favicon.ico">
+  </head>
+  <body>テスト</body>
+</html>
+`;
 
-Deno.test("parsedMeta", async () => {
-  const fetchStub = stub(
-    globalThis,
-    "fetch",
-    () =>
-      Promise.resolve(
-        new Response(HTML_DATA, { "headers": { "Content-Type": "text/html" } }),
-      ),
-  );
+Deno.test("parsedMeta", async (t) => {
+  await t.step("Full OGP tags", async () => {
+    const fetchStub = stub(
+      globalThis,
+      "fetch",
+      () =>
+        Promise.resolve(
+          new Response(FULL_OGP_HTML_DATA, {
+            "headers": { "Content-Type": "text/html" },
+          }),
+        ),
+    );
 
-  try {
-    const meta = await parsedMeta("https://example.com");
-    assertEquals<MetaData>(meta, {
-      description: "テスト",
-      favicon: "/favicon.ico",
-      open_graph: {
-        description: "テスト",
-        image: "https://example.com/example.png",
-        site_name: "テスト",
+    try {
+      const meta = await parsedMeta("https://example.com");
+      assertEquals<MetaData>(meta, {
         title: "テスト",
-        type: "website",
-        url: "https://example.com",
-      },
-      title: "テスト",
-      twitter: {
-        card: "summary",
         description: "テスト",
-        image: "https://example.com/example.png",
-        site: "@Twitter",
+        favicon: "/favicon.ico",
+        open_graph: {
+          description: "テスト",
+          image: "https://example.com/example.png",
+          site_name: "テスト",
+          title: "テスト",
+          type: "website",
+          url: "https://example.com",
+        },
+        twitter: {
+          card: "summary",
+          description: "テスト",
+          image: "https://example.com/example.png",
+          site: "@Twitter",
+          title: "テスト",
+        },
+      });
+    } finally {
+      fetchStub.restore();
+    }
+  });
+
+  await t.step("No OGP tags", async () => {
+    const fetchStub = stub(
+      globalThis,
+      "fetch",
+      () =>
+        Promise.resolve(
+          new Response(NO_OGP_HTML_DATA, {
+            "headers": { "Content-Type": "text/html" },
+          }),
+        ),
+    );
+
+    try {
+      const meta = await parsedMeta("https://example.com");
+      assertEquals<MetaData>(meta, {
         title: "テスト",
-      },
-    });
-  } finally {
-    fetchStub.restore();
-  }
+        description: "テスト",
+        favicon: "/favicon.ico",
+        open_graph: {
+          description: null,
+          image: null,
+          site_name: null,
+          title: null,
+          type: null,
+          url: null,
+        },
+        twitter: {
+          card: null,
+          description: null,
+          image: null,
+          site: null,
+          title: null,
+        },
+      });
+    } finally {
+      fetchStub.restore();
+    }
+  });
 });
